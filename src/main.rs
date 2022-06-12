1e7e76d63d7247ea3a73b31679bfa75717afdd16
Sven Allers <sven.allers@gmx.de>
2022-06-12 23:14:00 +0200

    Initial test track

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .insert_resource(LevelSelection::Index(0))
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("racing-track.ldtk"),
+        ..Default::default()
+    });
+}
